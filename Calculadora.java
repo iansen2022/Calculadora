2cb0a11075f5def635f81d88dc944450ba6fc9ea
Samuel Iansen <iansen2022@gmail.com>
2023-03-13 23:45:10 -0300

    calculador lendo dados

---
+++ Calculadora.java
@@ -0,0 +1,18 @@
+package calculadora;
+
+import java.util.Scanner;
+public class Calculadora {
+
+	public static void main(String[] args) {
+		double n1, n2;
+		double soma, sutracao, multiplicacao, divisao;
+		Scanner entrada = new Scanner (System.in);
+		
+		System.out.println("Informe o primeiro valor: ");
+		n1 = entrada.nextDouble();
+		System.out.println("Informe o segundo valor: ");
+		n2 = entrada.nextDouble();
+
+	}
+
+}
