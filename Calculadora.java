436a35d3176efbafeccdb7e12d15a0bf75874abc
Samuel Iansen <iansen2022@gmail.com>
2023-03-15 23:19:46 -0300

    operacao soma

--- Calculadora.java
+++ Calculadora.java
@@ -6,12 +6,30 @@ public class Calculadora {
 	public static void main(String[] args) {
 		double n1, n2;
 		double soma, sutracao, multiplicacao, divisao;
+		int op;
 		Scanner entrada = new Scanner (System.in);
 		
 		System.out.println("Informe o primeiro valor: ");
 		n1 = entrada.nextDouble();
 		System.out.println("Informe o segundo valor: ");
 		n2 = entrada.nextDouble();
+		
+		System.out.println("######Selecione uma operação#####");
+		System.out.println("[1]  -  Somar");
+		System.out.println("[2]  -  Subtrair");
+		System.out.println("[3]  -  Multiplicar");
+		System.out.println("[4]  -  Dividir");
+		System.out.println(">>>>>>>>Digite sua opção");
+		op = entrada.nextInt();
+		
+		switch (op) {
+		case 1: 
+			soma = n1+n2;
+			System.out.println("A soma é: "+soma);
+			break;
+
+		}
+		
 
 	}
 
